47c3381dd383d8e8bf7acb496b560529f5007492
evergreen <dany2zverev@gmail.com>
2021-11-24 14:49:07 +0300

    add Profile

---
+++ src/components/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-button class="profile-btn" text style="font-size: 45px;">
+      <n-icon>
+        <profile-icon/>
+      </n-icon>
+  </n-button>
+</template>
+
+<script>
+import { UserCircle as ProfileIcon } from '@vicons/fa'
+
+export default {
+  name: "PersonalAccount",
+  components: {
+    ProfileIcon
+  },
+}
+</script>
+
+<style scoped>
+.profile-btn {
+  margin-right: 30px;
+  transition: color .3s;
+}
+
+.profile-btn:hover {
+  color: #4FC08D;
+}
+</style>
