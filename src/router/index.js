9101f3df3fcea8217bf5cd97ffdd1ece7135b6e9
evergreen <dany2zverev@gmail.com>
2021-11-29 14:57:41 +0300

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage";
+import FilmListPage from "../pages/FilmListPage";
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "Home",
-  //   component: Home,
-  // },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  // },
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/films",
+    name: "Films",
+    component: FilmListPage,
+  },
+  {
+    path: "/online-cinema",
+    name: "Online Cinema",
+    component: FilmListPage,
+  },
+  {
+    path: "/cinema-rent",
+    name: "Cinema Rent",
+    component: FilmListPage,
+  },
 ];
 
 const router = createRouter({
