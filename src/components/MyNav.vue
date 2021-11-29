b738f0291631247c2ea3b676aa262c8c3557981f
evergreen <dany2zverev@gmail.com>
2021-11-29 14:57:25 +0300

    renamed Nav to MyNav

---
+++ src/components/MyNav.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="menu-line">
+    <nav class="wrap menu-line__nav">
+      <router-link :to='ref.value' v-for='ref in refs' :key='ref.value' tag="li">
+        <button :value="ref.value">
+          {{ ref.label }}
+        </button>
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default ({
+  name: 'my-nav',
+  data() {
+    return {
+      refs: [
+        {
+          value: "/online-cinema",
+          label: "Онлайн-кинотеатр",
+        },
+        {
+          value: '/films',
+          label: 'Фильмы'
+        },
+        {
+          value: '/cinema-rent',
+          label: 'Аренда кинозала'
+        }
+      ]
+    }
+  }
+})
+</script>
+
+<style scoped>
+.menu-line {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 57px;
+  font-size: 20px;
+  background-color: #F6F6F6;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+}
+
+.menu-line__nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.menu-line__nav button {
+  width: auto;
+  height: 50px;
+  padding-left: 32px;
+  padding-right: 32px;
+  font-size: 20px;
+  color: #4F5959;
+  border: 0;
+  background-color: transparent;
+  transition: transform .5s;
+}
+
+.menu-line__nav button:hover {
+  transform: scale(1.1)
+}
+
+.menu-line__nav .router-link-active:hover,
+.menu-line__nav .router-link-active button,
+.menu-line__nav .router-link-exact-active button{
+ color: #4FC08D;
+}
+
+@media (max-width: 736px) {
+  .menu-line__nav button {
+    font-size: 19px;
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+}
+
+@media (max-width: 487px) {
+  .menu-line {
+    height: 110px;
+  }
+
+  .menu-line__nav {
+    flex-direction: column;
+  }
+
+  .menu-line__nav a {
+    width: 100%;
+  }
+
+  .menu-line__nav button {
+    width: 100%;
+    height: 36px;
+  }
+}
+</style>
