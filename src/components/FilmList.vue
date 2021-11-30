8e01dfef21cd9ec6b85c3b4a56ddb7e1bbab3bf9
evergreen <dany2zverev@gmail.com>
2021-11-30 13:53:21 +0300

    renamed FilmListCrop to FilmList

---
+++ src/components/FilmList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="films-sort">
+    <div @click="setSortFilms(true)" class='btn' :class='{ active: isReleasedFilms, inactive: !isReleasedFilms }'>
+      <span>Сегодня в кино</span>
+    </div>
+    <div @click="setSortFilms(false)" class='btn' :class='{ active: !isReleasedFilms, inactive: isReleasedFilms }'>
+      <span>Скоро в кино</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilmListCrop",
+  data() {
+    return {
+      isReleasedFilms: true,
+    }
+  },
+  methods: {
+    setSortFilms(bool) {
+      this.isReleasedFilms = bool;
+    }
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+.films-sort {
+  width: 444px;
+  display: flex;
+  gap: 20px;
+  margin: 38px auto 0;
+}
+
+.films-sort div {
+  width: 100%;
+  height: 40px;
+  color: #333333;
+  display: flex;
+  justify-content: center;
+}
+
+.active {
+  border-bottom: 4px solid #4FC08D;
+  transition: transform 0.5s;
+}
+
+.active:hover {
+  transform: scale(1.1);
+}
+
+.inactive {
+  transition: transform 0.5s, color 0.3s;
+}
+
+.inactive:hover {
+  transform: scale(1.1);
+  color: #4FC08D;
+}
+</style>
