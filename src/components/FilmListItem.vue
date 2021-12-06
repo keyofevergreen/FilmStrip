fe07076963398a6fbb5d255d2e966db5e97fbabc
evergreen <dany2zverev@gmail.com>
2021-12-06 16:59:17 +0300

    remove fetching data and replace rating with duration

--- src/components/FilmListItem.vue
+++ src/components/FilmListItem.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="film-list__item">
-    <div class="film-list__item-card">
-      <div class="item-card__rating">
-        {{ ageLimits }}
-      </div>
-        <img :src="film.posterUrlPreview" :alt="film.nameRu" class="item-card__poster">
+  <div v-if="!isFetching">
+    <div class="film-list__item-rating">
+      {{ setDuration(film.duration) }}
     </div>
+    <img :src="film.posterUrlPreview" :alt="film.nameRu" class="film-list__item-poster">
     <div class="film-list__item-genres">
       <span v-for="genre in film.genres" :key="genre.genre">{{ genre.genre }}</span>
     </div>
     <h4 class="film-list__item-name">{{ film.nameRu }}</h4>
+  </div>
+  <div v-else class="film-list__item-fetching">
+    <div class="item-fetching__rating">Loading</div>
+    <div class="item-fetching__poster"></div>
   </div>
 </template>
 
@@ -17,72 +19,82 @@
 export default {
   name: 'FilmListItem',
   props: {
-    id: Number,
+    film: Object,
   },
   data() {
-    return {
-      film: {},
-      ageLimits: ''
-    }
+    return {}
   },
   methods: {
-    setFilm(film) {
-      this.film = film;
-    },
-    fetchFilm(id) {
-      fetch(`https://kinopoiskapiunofficial.tech//api/v2.2/films/${id}`, {
-        method: 'GET',
-        headers: {
-          'X-API-KEY': 'bbd5c8d2-662f-428b-9b73-5fb961a663ad',
-          'Content-Type': 'application/json',
-        },
-      })
-          .then(res => res.json())
-          .then(film => {
-            this.setFilm(film)
-          })
-          .catch(err => console.log(err));
-    },
-    setAgeRatingLimits(film) {
-      if(!film.ratingAgeLimits) {
-        return '0+'
-      } else {
-        return `${film.ratingAgeLimits.match(/\d[1-9]/g)}+`
+    setDuration(duration) {
+      const lastSymbol = duration.toString().slice(-1);
+      if(lastSymbol === '1') {
+        return `${duration} минута`
       }
-    }
-  },
-  mounted() {
-    this.fetchFilm(this.id)
-  },
-  watch: {
-    film(newValue) {
-      this.ageLimits = this.setAgeRatingLimits(newValue);
-    }
+      if(lastSymbol > 1 && lastSymbol < 5) {
+        return `${duration} минуты`
+      }
+      return `${duration} минут`
+    },
+    // setFetching(bool) {
+    //   this.isFetching = bool
+    // },
+    // setFilm(film) {
+    //   this.film = film;
+    // },
+    // async fetchFilm(id) {
+    //   try {
+    //     this.setFetching(true);
+    //     const response = await fetch(`https://kinopoiskapiunofficial.tech//api/v2.2/films/${id}`, {
+    //       method: 'GET',
+    //       headers: {
+    //         'X-API-KEY': 'bbd5c8d2-662f-428b-9b73-5fb961a663ad',
+    //         'Content-Type': 'application/json',
+    //       },
+    //     });
+    //     const film = await response.json();
+    //     await this.setFilm(film)
+    //   } catch (e) {
+    //     console.error(e)
+    //   } finally {
+    //     this.setFetching(false);
+    //   }
+    // },
+    //   setAgeRatingLimits(film) {
+    //     if (film.ratingAgeLimits === null) {
+    //       return '0+'
+    //     } else {
+    //       return `${film.ratingAgeLimits.match(/\d{1,2}/g)}+`
+    //     }
+    //   }
+    // },
+    // mounted() {
+    //   this.fetchFilm(this.id)
+    // },
+    // watch: {
+    //   film(newValue) {
+    //     this.ageRatingLimits = this.setAgeRatingLimits(newValue);
+    //   }
   }
 }
 </script>
 
 <style scoped>
-.film-list__item-card {
-  width: 212px;
-  margin-bottom: 10px;
-}
-
-.item-card__rating {
+.film-list__item-rating {
   height: 32px;
   font-size: 20px;
   color: #fff;
-  background-color: #4FC08D;
+  background-color: var(--green);
 
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-.item-card__poster {
+.film-list__item-poster {
   display: inline-block;
   width: 100%;
   height: 300px;
+  margin-bottom: 10px;
 }
 
 .film-list__item-genres {
@@ -91,12 +103,40 @@ export default {
   gap: 5px;
   font-size: 12px;
   font-weight: 500;
-  color: #999DA5;
+  color: var(--gray);
+  margin-bottom: 10px;
 }
 
 .film-list__item-name {
   font-size: 18px;
   font-weight: 500;
-  color: #333333;
+  color: var(--pretty-black);
+}
+
+.film-list__item-fetching {
+  height: 332px;
+  border: 4px dashed var(--pretty-black);
+}
+
+.item-fetching__rating {
+  height: 32px;
+  font-size: 20px;
+  border-bottom: 4px dashed var(--pretty-black);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.item-fetching__poster {
+  width: 100%;
+  height: 100%;
+  background-image: url(../assets/chaplin.svg);
+  background-position: center center;
+  background-repeat: no-repeat;
+  -webkit-background-size: 100% 100%;
+  -moz-background-size: 100% 100%;
+  -o-background-size: 100% 100%;
+  background-size: 100% 100%;
 }
 </style>
