a0f8e27491f43a9f1093702ca4e17dc34676178e
evergreen <dany2zverev@gmail.com>
2021-12-07 16:13:12 +0300

    refactoring code for better readability

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,13 +1,17 @@
 <template>
+  <my-select v-if="mode === 'full'"></my-select>
   <sort-by-release-buttons/>
-  <div v-if="mode === 'crop'" class="film-list-wrap">
+  <div v-if="mode === 'full'">
+    <div v-if="isReleasedFilms" class="film-list-full">
+      <film-list-item v-for="film in releasedFilms" :key="film.kinopoiskId" :film="film"></film-list-item>
+    </div>
+    <div v-else class="film-list-full">
+      <film-list-item v-for="film in unreleasedFilms" :key="film.kinopoiskId" :film="film"></film-list-item>
+    </div>
+  </div>
+  <div v-else class="film-list-crop">
     <my-film-swiper :filmList="releasedFilms" v-if="isReleasedFilms"/>
     <my-film-swiper :filmList="unreleasedFilms" v-else/>
-  </div>
-  <div v-else>
-    <div v-if="isReleasedFilms">
-      <film-list-item v-for="film in releasedFilms.slice(0, 5)" :key="film.kinopoiskId" :id="film.kinopoiskId"></film-list-item>
-    </div>
   </div>
 </template>
 
@@ -17,16 +21,18 @@ import { mapGetters, mapState } from 'vuex';
 import SortByReleaseButtons from './SortByReleaseButtons';
 import MyFilmSwiper from './UI/MyFilmSwiper';
 import FilmListItem from './FilmListItem';
+import MySelect from './UI/MySelect';
 
 export default {
   name: 'FilmList',
   props: {
-    mode: String
+    mode: String,
   },
   components: {
     SortByReleaseButtons,
     MyFilmSwiper,
-    FilmListItem
+    FilmListItem,
+    MySelect
   },
   computed: {
     ...mapState({
@@ -36,12 +42,20 @@ export default {
       releasedFilms: 'getReleasedFilms',
       unreleasedFilms: 'getUnreleasedFilms'
     })
-  }
+  },
 }
 </script>
 
 <style scoped>
-.film-list-wrap {
-  height: 605px;
+.film-list-full {
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(210px, 1fr));
+}
+
+@media (max-width: 440px) {
+  .film-list-full {
+    padding: 0 80px;
+  }
 }
 </style>
