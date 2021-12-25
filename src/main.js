00d11a33301541f30187f65550cdda6d5faafdd4
evergreen <dany2zverev@gmail.com>
2021-12-25 21:43:05 +0300

    Add Antd imports

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import naive from "naive-ui";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.less';
 
-createApp(App).use(naive).use(store).use(router).mount("#app");
+createApp(App).use(naive).use(Antd).use(store).use(router).mount("#app");
