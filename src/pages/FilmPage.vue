f1386fdd849d945ce45e5acff478a7423305068b
evergreen <dany2zverev@gmail.com>
2021-12-25 21:41:52 +0300

    Add FilmPage

--- src/pages/FilmPage.vue
+++ src/pages/FilmPage.vue
@@ -1,102 +1,110 @@
 <template>
-  <div>
-    Это страница фильма с ID {{ $route.params.id }}
-    <film-item :film="filmData" :trailerURL="trailerURL"></film-item>
+  <div v-if="selectedFilm && !isFetching">
+    <film-item :film="selectedFilm" :trailerURL="trailerURL"></film-item>
+    <div class="data-line">
+      <span>Расписание на сегодня</span>
+    </div>
+    <session-schedule :cinemas="cinemas"></session-schedule>
   </div>
 </template>
 
 <script>
 import FilmItem from '../components/FilmItem';
+import SessionSchedule from '../components/SessionSchedule';
+import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'FilmPage',
   components: {
-    FilmItem
+    FilmItem,
+    SessionSchedule
   },
   props: {
     id: String
   },
+  computed: {
+    ...mapState({
+      selectedFilm: state => state.selectedFilm,
+      trailerURL: state => state.trailersUrlOfSelectedFilm,
+      ratingAgeLimits: state => state.ageLimitsOfSelectedFilm,
+      cinemas: state => state.cinemas
+    })
+  },
   data() {
     return {
-      filmData: {},
-      trailerURL: null,
-      isFetching: false
+      isFetching: false,
     }
   },
   methods: {
-    setDuration(duration) {
-      const lastSymbol = duration.toString().slice(-1);
-      if (lastSymbol === '1') {
-        return `${duration} минута`
-      }
-      if (lastSymbol > 1 && lastSymbol < 5) {
-        return `${duration} минуты`
-      }
-      return `${duration} минут`
-    },
+    ...mapMutations({
+      setFilmData: 'setSelectedFilm',
+      setTrailersUrl: 'setTrailerUrlOfSelectedFilm',
+      setAgeLimits: 'setAgeLimitsOfSelectedFilm'
+    }),
     setFetching(bool) {
       this.isFetching = bool;
     },
-    setFilmData(film) {
-      this.filmData = film;
-    },
-    setTrailerData(trailers) {
-      const trailer = trailers?.items.filter(trailer => trailer.site === 'YOUTUBE' && /(Трейлер)/i.test(trailer.name));
-      if(trailer.length) {
-        const regex = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)?/;
-        const url = trailer[0]?.url;
-        this.trailerURL = `https://www.youtube.com/embed/${url.match(regex)[1]}`;
-      }
-    },
     async fetchFilmsData(id) {
+      const fetchData = (url) => {
+        return fetch(url, {
+          method: 'GET',
+          headers: {
+            'X-API-KEY': 'bbd5c8d2-662f-428b-9b73-5fb961a663ad',
+            'Content-Type': 'application/json',
+          },
+        })
+      }
+      const getCorrectYoutubeTrailerUrl = (trailers) => {
+        const trailer = trailers?.items.filter(trailer => trailer.site === 'YOUTUBE' && /(Трейлер)/i.test(trailer.name));
+        if (trailer.length) {
+          const regex = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)?/;
+          const url = trailer[0]?.url;
+          return `https://www.youtube.com/embed/${url.match(regex)[1]}`;
+        }
+      }
+      const getAgeRatingLimits = (film) => {
+        if (film.ratingAgeLimits === null) {
+          return '0+'
+        } else {
+          return `${film.ratingAgeLimits.match(/\d{1,2}/g)}+`
+        }
+      }
+
       try {
         this.setFetching(true);
-        const filmData = await fetch(`https://kinopoiskapiunofficial.tech//api/v2.2/films/${id}`, {
-          method: 'GET',
-          headers: {
-            'X-API-KEY': 'bbd5c8d2-662f-428b-9b73-5fb961a663ad',
-            'Content-Type': 'application/json',
-          },
-        });
+        const filmData = await fetchData(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`);
         const film = await filmData.json();
-        await this.setFilmData(film)
 
-        const trailerData = await fetch(`https://kinopoiskapiunofficial.tech//api/v2.2/films/${id}/videos`, {
-          method: 'GET',
-          headers: {
-            'X-API-KEY': 'bbd5c8d2-662f-428b-9b73-5fb961a663ad',
-            'Content-Type': 'application/json',
-          },
-        });
-        const trailer = await trailerData.json();
-        await this.setTrailerData(trailer)
+        const trailerData = await fetchData(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}/videos`);
+        const trailers = await trailerData.json();
+        await this.setFilmData(film);
+        await this.setAgeLimits(getAgeRatingLimits(film));
+        await this.setTrailersUrl(getCorrectYoutubeTrailerUrl(trailers));
 
       } catch (e) {
         console.error(e)
       } finally {
         this.setFetching(false);
       }
-    },
-
-    setAgeRatingLimits(film) {
-      if (film.ratingAgeLimits === null) {
-        return '0+'
-      } else {
-        return `${film.ratingAgeLimits.match(/\d{1,2}/g)}+`
-      }
     }
   },
   mounted() {
     this.fetchFilmsData(this.id);
-  },
-  watch: {
-    film(newValue) {
-      this.ageRatingLimits = this.setAgeRatingLimits(newValue);
-    }
   }
 }
 </script>
 
 <style scoped>
+.data-line {
+  height: 46px;
+  margin-top: 20px;
+  padding-left: 20px;
+  font-size: 20px;
+  color: #fff;
+  background-color: var(--green);
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
 
+  display: flex;
+  align-items: center;
+}
 </style>
