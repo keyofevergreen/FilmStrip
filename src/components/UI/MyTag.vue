0b3f3d877ea654dd20e72aced4475cdb30efd3d6
evergreen <dany2zverev@gmail.com>
2021-12-25 20:23:05 +0300

    Add MyTag

---
+++ src/components/UI/MyTag.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="tag">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'MyOldRatingTag',
+  props: {
+    text: {
+      type: String, Number
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tag {
+  height: 27px;
+  padding: 0 10px;
+  width: 60px;
+  font-size: 17px;
+  font-weight: 300;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
