c68fc6ff5d5d659761344bdf6d665d204f9075b8
evergreen <dany2zverev@gmail.com>
2021-12-25 20:22:38 +0300

    Add description and information about movie in layout

--- src/components/FilmItem.vue
+++ src/components/FilmItem.vue
@@ -1,22 +1,63 @@
 <template>
-  <div v-if="trailerURL" class="film-item">
-    <iframe class="film-item__frame" :src="trailerURL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <div class="film-item__text">
-      <h1>{{film.nameRu}}</h1>
-      <p>{{film.nameOriginal}} ({{film.year}}, {{film.countries.map(country => country.country).join(', ')}})</p>
+  <div class="film-item">
+    <iframe v-if="trailerURL" class="film-item__frame" :src="trailerURL" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+    <div v-else class='film-item__poster'>
+      <img :src="film.posterUrlPreview" :alt="`Постер ${film.nameRu}`">
+    </div>
+    <div class="film-item__content">
+      <h1 class="film-item__header">{{ film.nameRu }}</h1>
+      <p class="film-item__country">{{ film.nameOriginal }} ({{ film.year }},
+        {{ film.countries.map(country => country.country).join(', ') }})</p>
+      <div class="flex film-item__tags">
+        <my-tag :text="ageLimits" class="film-item__tags-age-limit"></my-tag>
+        <my-tag
+            v-if="film.ratingImdb || film.ratingKinopoisk"
+            :text="film.ratingImdb || film.ratingKinopoisk"
+            class="film-item__tags-rating">
+        </my-tag>
+      </div>
+      <p class="film-item__genres">{{ film.genres.length > 1 ? 'Жанры: ' : 'Жанр: ' }}
+        {{ film.genres.map(genre => genre.genre).join(', ') }}</p>
+      <p v-if="film.filmLength" class="film-item__duration">Длительность: {{ setDuration(film.filmLength) }}</p>
+      <p class="film-item__description">{{ film.description }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import MyTag from './UI/MyTag';
+import { mapState } from 'vuex';
+
 export default {
   name: 'FilmItem',
+  components: {
+    MyTag
+  },
   props: {
     trailerURL: {
       type: String
     },
     film: {
       type: Object
+    }
+  },
+  computed: {
+    ...mapState({
+      ageLimits: state => state.ageLimitsOfSelectedFilm
+    })
+  },
+  methods: {
+    setDuration(duration) {
+      const lastSymbol = duration.toString().slice(-1);
+      if (lastSymbol === '1') {
+        return `${duration} минута`
+      }
+      if (lastSymbol > 1 && lastSymbol < 5) {
+        return `${duration} минуты`
+      }
+      return `${duration} минут`
     }
   }
 }
@@ -25,26 +66,75 @@ export default {
 <style scoped>
 .film-item {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(310px, 1fr));
   gap: 20px;
 }
 
 .film-item__frame {
   width: 100%;
   height: 300px;
+  border-radius: 10px;
 }
 
-.film-item__text {
+.film-item__poster {
+  display: flex;
+  justify-content: center;
+}
+
+.film-item__poster img {
+  max-height: 480px;
+  border-radius: 5px;
+}
+
+.film-item__content {
   font-size: 15px;
 }
 
-.film-item__text h1 {
+.film-item__header {
   font-size: 30px;
 }
 
-@media(max-width: 440px) {
+.film-item__country {
+  color: var(--grey);
+  margin-bottom: 15px;
+}
+
+.film-item__genres {
+  margin-bottom: 5px;
+}
+
+.film-item__duration {
+  margin-bottom: 15px;
+}
+
+.film-item__tags {
+  margin-bottom: 5px;
+}
+
+.film-item__tags-age-limit {
+  border: 1px dashed var(--pretty-black);
+}
+
+.film-item__tags-rating {
+  color: #fff;
+  background-color: var(--green);
+}
+
+@media (max-width: 440px) {
   .film-item__frame {
     height: 200px;
+  }
+}
+
+@media (max-width: 739px) {
+  .film-item {
+    padding: 0 20px;
+  }
+}
+
+@media (max-width: 310px) {
+  .film-item {
+    grid-template-columns: repeat(auto-fit, minmax(305px, 1fr));
   }
 }
 </style>
