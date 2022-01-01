fc3cee566c2475c5e3c853a195dfd8bfac1fc893
evergreen <dany2zverev@gmail.com>
2022-01-01 19:38:16 +0300

    add Seat

---
+++ src/components/Seat.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="`${contains(tickets, id) ? 'selected' : ''} ${isLimit ? 'locked' : ''} ${isOccupied ? 'occupied' : ''}`" @click="handlerChange()"></div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapState } from 'vuex';
+
+export default {
+  name: 'Seat',
+  props: {
+    row: {
+      type: Number
+    },
+    seatInRow: {
+      type: Number
+    },
+    isOccupied: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      id: `${this.row}-${this.seatInRow}`
+    }
+  },
+  methods: {
+    ...mapMutations({
+      setTicket: 'setTicket',
+      removeTicket: 'removeTicket'
+    }),
+    contains(tickets, id) {
+      return tickets.find((ticket) => ticket.id === id);
+    },
+    handlerChange() {
+      const isContains = this.tickets.length ? this.contains(this.tickets, this.id) : false;
+      if (this.isLimit) {
+        if (isContains) {
+          this.removeTicket(this.id);
+        }
+      } else {
+        if (isContains) {
+          this.removeTicket(this.id);
+        } else {
+          this.setTicket({row: this.row, seat: this.seatInRow, id: this.id});
+        }
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      tickets: state => state.selectedTickets
+    }),
+    ...mapGetters({
+      isLimit: 'getIsLimitOfTickets'
+    })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
