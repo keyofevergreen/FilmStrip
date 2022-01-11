708850795c77bab675a3d54335ad56f6e52f6270
evergreen <dany2zverev@gmail.com>
2022-01-11 21:35:02 +0300

    Changed configuration for publishing on gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
+  publicPath: '/FilmStrip/',
   css: {
     loaderOptions: {
       less: {
         lessOptions: {
-          // If you are using less-loader@5 please spread the lessOptions to options directly
           modifyVars: {
             'primary-color': '#1DA57A',
             'link-color': '#1DA57A',
