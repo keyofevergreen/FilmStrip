f2d0a6bc9d195a1dd8ad624bf2586356ec853dd9
evergreen <dany2zverev@gmail.com>
2022-01-18 14:19:20 +0300

    add validators

--- src/functions/validators.js
+++ src/functions/validators.js
@@ -1,3 +1,20 @@
-const hasValidPhoneNumber = val => /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(val);
+//eslint-disable-next-line
 
-export {hasValidPhoneNumber}
+
+const hasValidPhoneNumber = (value) => /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){11}(\s*)?$/.test(value);
+const hasUnregisteredMail = (mail) => {
+  const data = localStorage.getItem('users');
+  const users = JSON.parse(data);
+  return !users.find(user => user.mail === mail);
+};
+const hasRegisteredMail = (mail) => {
+  const data = localStorage.getItem('users');
+  const users = JSON.parse(data);
+  return !!users.find(user => user.mail === mail);
+};
+const hasUnregisteredPhoneNumber = (phoneNumber) => {
+  const data = localStorage.getItem('users');
+  const users = JSON.parse(data);
+  return !users.find(user => user.phoneNumber === phoneNumber);
+};
+export { hasValidPhoneNumber, hasUnregisteredMail, hasRegisteredMail, hasUnregisteredPhoneNumber };
