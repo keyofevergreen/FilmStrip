0a00244d815d1ed3af1209957fe06dba0d9459cf
evergreen <dany2zverev@gmail.com>
2022-01-18 14:09:21 +0300

    refactoring AuthModal for AuthForm/RegistrationForm

--- src/components/AuthModal/AuthModal.vue
+++ src/components/AuthModal/AuthModal.vue
@@ -1,120 +1,60 @@
 <template>
   <a-modal v-model:visible="isVisible" :centered="true" width="420px" :footer="null" :afterClose="handleCancel"
            :closable="false" wrapClassName="modal">
+    <div v-if="currentModalPage === 0">
+      <div class="registration-link-wrap">
+        <a class="auth-header auth-header-registration-link" @click="linkToRegistrationForm">Регистрация</a>
+      </div>
       <h2 class="auth-header">Войти</h2>
-      <h2 class="auth-header">Войти</h2>
-    <form>
-      <label>
-        <a-input class="payment-form__input"
-                 v-model:value="form.userMail"
-                 @blur="v$.form.userMail.$touch"
-                 type="email"
-                 placeholder="Введите вашу почту"
-                 pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-        />
-        <p v-if="v$.form.userMail.$error" class="error-message">
-          {{ userMailErrors }}
-        </p>
-      </label>
-      <label>
-        <a-input class="payment-form__input"
-                 v-model:value="form.userPassword"
-                 @blur="v$.form.userPassword.$touch"
-                 type="email"
-                 placeholder="Введите вашу почту"
-                 pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-        />
-        <p v-if="v$.form.userPassword.$error" class="error-message">
-          {{ userPasswordErrors }}
-        </p>
-      </label>
-    </form>
-    <div class="btn-wrap">
-      <a-spin :spinning="isFetching">
-        <button class="btn btn-auth" @click="handleOk">Тестовый вход</button>
-      </a-spin>
+      <auth-form/>
+    </div>
+    <div v-else>
+      <h2 class="auth-header">Регистрация</h2>
+      <registration-form/>
     </div>
   </a-modal>
 </template>
 
 <script>
 import { mapMutations, mapState } from 'vuex';
-import useVuelidate from '@vuelidate/core';
-import { email, required } from '@vuelidate/validators';
+import AuthForm from './AuthForm';
+import RegistrationForm from './RegistrationForm';
 
 export default {
   name: 'AuthModal',
-  setup() {
-    return { v$: useVuelidate() };
-  },
+  components: { AuthForm, RegistrationForm },
   data() {
     return {
       isVisible: false,
-      isFetching: false,
-      form: {
-        userMail: '',
-        userPassword: '',
-      }
-    };
-  },
-  validations() {
-    return {
-      form: {
-        userMail: { required, email },
-        userPassword: { required },
-      }
-    };
+      currentModalPage: 0
+    }
   },
   computed: {
     ...mapState({
-      loginModalVisible: state => state.loginModalVisible,
+      authModalVisible: state => state.authModalVisible,
     }),
-    userMailErrors() {
-      if (this.v$.form.userMail.required.$invalid) return '* Обязательно для заполнения.';
-      if (this.v$.form.userMail.email.$invalid) return '* Неправильный формат почты.';
-      return null;
-    },
-    userPasswordErrors() {
-      if (this.v$.form.userPassword.required.$invalid) return '* Обязательно для заполнения.';
-      return null;
-    },
   },
   methods: {
     ...mapMutations({
-      setLoginModalVisible: 'setLoginModalVisible',
-      setAuth: 'setAuth'
+      setAuthModalVisible: 'setAuthModalVisible'
     }),
-    setFetching(bool) {
-      this.isFetching = bool;
-    },
-    handleOk() {
-      this.setFetching(true);
-      try {
-        setTimeout(() => {
-          this.setAuth(true);
-          this.setLoginModalVisible(false)
-          this.setFetching(false);
-        }, 2000);
-      } catch (e) {
-        console.error(e);
-      }
-
-    },
     handleCancel() {
-      this.setLoginModalVisible(false);
-      this.form.userMail = '';
-      this.form.userPassword = '';
+      this.setAuthModalVisible(false);
+      this.currentModalPage = 0;
+    },
+    linkToRegistrationForm() {
+      this.currentModalPage = 1;
     }
   },
   watch: {
-    loginModalVisible(newValue) {
+    authModalVisible(newValue) {
       this.isVisible = (newValue);
     }
   },
   beforeMount() {
     if (this.isVisible === true) {
       this.isVisible = false;
-      this.setLoginModalVisible(false);
+      this.setAuthModalVisible(false);
     }
   }
 };
@@ -130,21 +70,17 @@ export default {
   color: var(--grey);
 }
 
-.btn-wrap {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+.registration-link-wrap {
+  text-align: right;
 }
 
-.btn-auth {
-  width: 100%;
-  min-height: 45px;
-  border: 1px solid var(--green);
-  border-radius: 5px;
-  transition: border 0.4s;
+.auth-header-registration-link {
+  font-size: 19px;
+  color: var(--green);
+  transition: color 0.4s;
 }
 
-.btn-auth:hover {
-  border: 1px solid var(--light-green);
+.auth-header-registration-link:hover {
+  color: var(--light-green);
 }
 </style>
