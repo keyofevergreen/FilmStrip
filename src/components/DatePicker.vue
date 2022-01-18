b6d72fe2d7cc57997996fb777e0a15e392717227
evergreen <dany2zverev@gmail.com>
2022-01-18 14:16:26 +0300

    adding dist subtree

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-date-picker
+      :locale="locale"
+      :inputReadOnly="true"
+      size="large"
+      format="DD-MM-YYYY"
+      :disabled-date="disabledDate"
+  />
+</template>
+
+<script>
+import moment from 'moment';
+import locale from 'ant-design-vue/es/date-picker/locale/ru_RU';
+
+export default {
+  name: 'DatePicker',
+  data() {
+    return {
+      locale
+    }
+  },
+  methods: {
+    disabledDate(current) {
+      // Can not select days before today and today
+      return current < moment().subtract(1, 'd') || current > moment().add(7, 'd');
+    },
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
