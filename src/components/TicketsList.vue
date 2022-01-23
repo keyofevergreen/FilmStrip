09af13a0845400200f6d506bcf35d43ee1c9a4fe
evergreen <dany2zverev@gmail.com>
2022-01-23 16:17:59 +0300

    add TicketsList

---
+++ src/components/TicketsList.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="tickets-list">
+  <tickets-list-item v-for="ticket in reversedTicketHistory" :key="ticket.id" :ticket="ticket" />
+</div>
+</template>
+
+<script>
+import TicketsListItem from './TicketsListItem';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'TicketsList',
+  components: { TicketsListItem },
+  computed: {
+    ...mapState({
+      ticketsHistory: state => state.auth.authAccount.ticketsHistory
+    }),
+    reversedTicketHistory() {
+      return this.ticketsHistory.map(el => el).reverse();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tickets-list {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px 10px;
+}
+</style>
