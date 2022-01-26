f315c8dca80e920f6b26efdda558b55dc694ebcd
evergreen <dany2zverev@gmail.com>
2022-01-26 20:39:03 +0300

    Added the ability to select a date

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,33 +1,47 @@
 <template>
   <a-date-picker
+      v-model:value="value"
+      @change="onChange"
       :locale="locale"
+      :disabled-date="disabledDate"
+      format="DD.MM.YYYY"
+      placeholder="Сегодня"
       :inputReadOnly="true"
       size="large"
-      format="DD-MM-YYYY"
-      :disabled-date="disabledDate"
-  />
+  >
+    <span>{{ time ? time : 'SelectTime' }}</span>
+  </a-date-picker>
 </template>
 
 <script>
+import { mapMutations, mapState } from 'vuex';
+import locale from 'ant-design-vue/es/date-picker/locale/ru_RU.js';
 import moment from 'moment';
-import locale from 'ant-design-vue/es/date-picker/locale/ru_RU';
 
 export default {
   name: 'DatePicker',
   data() {
     return {
-      locale
+      locale,
+      value: null
     }
   },
+  computed: {
+    ...mapState({
+      dateOfDatePicker: state => state.selectedFilm.dateOfDatePicker
+    })
+  },
   methods: {
+    ...mapMutations({
+      setDate: 'selectedFilm/setSelectedDate',
+    }),
     disabledDate(current) {
       // Can not select days before today and today
       return current < moment().subtract(1, 'd') || current > moment().add(7, 'd');
     },
+    onChange(date) {
+      this.setDate(date.format('DD.MM.YYYY'));
+    }
   }
 };
 </script>
-
-<style scoped>
-
-</style>
